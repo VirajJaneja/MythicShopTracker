1532af1fc4681f762e54c3ef58e1069a3c718a17
VirajJ <97782243+VirajJaneja@users.noreply.github.com>
2024-01-10 09:54:56 -0500

    made some changes

--- index.js
+++ index.js
@@ -1,40 +1,42 @@
 // Fetch JSON data from 'mainJSON.json'
 fetch('mainJSON.json')
-// Fetch JSON data from 'mainJSON.json'
-.then(response => response.json())
-.then(jsonData => {
-    // Create a table
-    const table = d3.select("#chart").append("table");
+    .then(response => response.json())
+    .then(jsonData => {
+        const table = d3.select("#chart").append("table").style("border", "1px solid #242c36"); 
 
-    // Append thead and tbody
-    const thead = table.append("thead");
-    const tbody = table.append("tbody");
+        const thead = table.append("thead");
+        const tbody = table.append("tbody");
 
-    // Get column headers from the first entry
-    const columns = ['Skin: ', ...Object.keys(jsonData[Object.keys(jsonData)[0]])];
+        const excludedColumns = ['4th Appearance', '5th Appearanace']; 
 
-    // Append the header row
-    thead.append("tr")
-        .selectAll("th")
-        .data(columns)
-        .enter()
-        .append("th")
-        .text(d => d);
+        const columns = ['Skin: ', ...Object.keys(jsonData[Object.keys(jsonData)[0]]).filter(key => !excludedColumns.includes(key))];
 
-    // Append the data rows
-    const rows = tbody.selectAll("tr")
-        .data(Object.entries(jsonData))
-        .enter()
-        .append("tr");
+        thead.append("tr")
+            .selectAll("th")
+            .data(columns)
+            .enter()
+            .append("th")
+            .text(d => d)
+            .style("color", (d, i) => i === 0 ? "#D0A85C" : "#D0A85C");
 
-    // Append cells with data
-    const cells = rows.selectAll("td")
-        .data(d => {
-            const title = d[0].toLowerCase().replace(/\s+/g, ' '); // Replace underscores with spaces
-            return [title, ...Object.values(d[1])];
-        })
-        .enter()
-        .append("td")
-        .text(d => d);
-})
-.catch(error => console.error('Error fetching JSON:', error));
+        const rows = tbody.selectAll("tr")
+            .data(Object.entries(jsonData))
+            .enter()
+            .append("tr");
+
+        const cells = rows.selectAll("td")
+            .data(d => {
+                const title = d[0].toLowerCase().replace(/\s+/g, ' ');
+                const formattedTitle = title.charAt(0).toUpperCase() + title.slice(1);
+
+                const updatedData = [formattedTitle, ...Object.values(d[1]).filter((value, index) => !excludedColumns.includes(Object.keys(d[1])[index]))].map(value => value === "0.0" ? "" : value);
+
+                return updatedData;
+            })
+            .enter()
+            .append("td")
+            .text(d => d)
+            .style("color", (d, i) => i === 0 ? "#D0A85C" : "white")
+            .style("text-align", (d, i) => i === 0 ? "left" : "center");
+    })
+    .catch(error => console.error('Error fetching JSON:', error));
