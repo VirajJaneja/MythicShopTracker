d86b44ffaeee067eb4522603fd4d00a9c14a11cd
VirajJ <97782243+VirajJaneja@users.noreply.github.com>
2024-01-11 04:09:32 -0500

    Made lot of eligibility col changes

--- index.js
+++ index.js
@@ -10,7 +10,8 @@ fetch('mainJSON.json')
         const excludedColumns = ['4th Appearance', '5th Appearanace']; 
 
         const columns = ['Skin: ', ...Object.keys(jsonData[Object.keys(jsonData)[0]]).filter(key => !excludedColumns.includes(key))];
-
+        const dateCol = 'Next Eligible:'
+        columns.push(dateCol);
         thead.append("tr")
             .selectAll("th")
             .data(columns)
@@ -28,9 +29,8 @@ fetch('mainJSON.json')
             .data(d => {
                 const title = d[0].toLowerCase().replace(/\s+/g, ' ');
                 const formattedTitle = title.charAt(0).toUpperCase() + title.slice(1);
-
-                const updatedData = [formattedTitle, ...Object.values(d[1]).filter((value, index) => !excludedColumns.includes(Object.keys(d[1])[index]))].map(value => value === "0.0" ? "" : value);
-
+                const newDateValue = calculateDate(d); 
+                const updatedData = [formattedTitle, ...Object.values(d[1]).filter((value, index) => !excludedColumns.includes(Object.keys(d[1])[index])), calculateDate(d)].map(value => value === "0.0" ? "" : value);
                 return updatedData;
             })
             .enter()
@@ -40,3 +40,51 @@ fetch('mainJSON.json')
             .style("text-align", (d, i) => i === 0 ? "left" : "center");
     })
     .catch(error => console.error('Error fetching JSON:', error));
+
+    // const div1Height = document.getElementById('chart').offsetHeight;
+    // document.getElementById('footer').style.marginTop = div1Height + 'px';
+
+
+    // const tableHeight = document.getElementById('chart').offsetHeight;
+
+    // document.getElementById('additionalDiv').style.height = tableHeight + 'px';
+
+
+async function calculateDate(data) {
+    const excludedColumns = ['4th Appearance', '5th Appearanace', 'Total Appearances'];
+    const appearancesList = Object.entries(data[1]).filter(([key]) => !excludedColumns.includes(key)).map(([key, value]) => value);
+
+    const patch = appearancesList.reduce((maxValue, currentValue) => {
+        return currentValue.localeCompare(maxValue) > 0 ? currentValue : maxValue;
+    }, '0.0');
+
+    let resultDate = "ISNT WORKING";
+
+    try {
+        const response = await fetch('patchDates.json');
+        const jsonData = await response.json();
+
+        const temp = patch + '';
+        const matchingObject = jsonData.find(obj => obj.patch === temp);
+
+        if (matchingObject) {
+            const correspondingDate = matchingObject.date;
+            resultDate = correspondingDate;
+        } else {
+            console.log("ISSUE WITH DATA: " + data);
+            return "Failed Process";
+        }
+
+
+        const currentDate = new Date(resultDate);
+
+        currentDate.setFullYear(currentDate.getFullYear() + 1);
+
+        const updatedDateString = currentDate.toLocaleDateString('en-US', { month: 'long', day: 'numeric', year: 'numeric' });
+
+        return updatedDateString;
+    } catch (error) {
+        console.error('Error fetching JSON:', error);
+        return "Failed Process";
+    }
+}
